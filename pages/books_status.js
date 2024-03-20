2044d75ef93dda63627ce3b99dfdcff996bc148f
nikkicohen <cohen.nik@northeastern.edu>
2024-03-20 17:01:10 -0400

    in class activities 3-14, completed 3-20

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,14 @@
 let BookInstance = require('../models/bookinstance');
 
 exports.show_all_books_status = function(res) {
-  return res.send([]);
-}
+  BookInstance.find({'status': {$eq: 'Available'}})
+  .populate('book')
+  .exec()
+  .then(list_bookinstances => {
+    res.send(list_bookinstances.map(function(bookinstance) {
+      return bookinstance.book.title + " : " + bookinstance.status;
+    }));
+  })
+  .catch(err => res.send('Status not found'));
+  //return res.send([]);
+};
